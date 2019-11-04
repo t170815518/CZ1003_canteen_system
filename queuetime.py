e8fe9358a5bbc5d44a4ffa1b209a7f37f81680be
Mimizume <56423918+Mimizume@users.noreply.github.com>
2019-11-05 01:37:53 +0800

    Update queuetime.py

--- queuetime.py
+++ queuetime.py
@@ -1,1 +1,11 @@
-from datetime import datetime
+def waiting_time():
+    while True:
+        try:
+            num_ppl = int(input("Enter no. of people in the queue: "))
+            wait_time = int(num_ppl)*3
+            print("Estimated waiting time is ", wait_time, " minutes.")
+            break
+        except:
+            print("Please enter a valid no.")
+            continue
+waiting_time()
