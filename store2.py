ec555e12ce8d288e16a469eb4a40932e5cde46d8
tanweeli <56423902+tanweeli@users.noreply.github.com>
2019-11-06 18:16:34 +0800

    Update store2.py

--- store2.py
+++ store2.py
@@ -7,23 +7,25 @@ store_menu = {
     "Salad" : ["Chicken Salad", "Fish Salad", "Salmon Salad"],
     "Starbuck's Coffee" : ["Americano", "Latte", "Cappucino"] }
 
-#store_name = "Western Food"
-#time = (2019,11,10,5,29) #yyyy,mm,dd,hh,mm
+#store_name = "Starbuck's Coffee"           Testing code
+#time = (2019,11,7,9,29) #yyyy,mm,dd,hh,mm  Testing code
 
-def get_menu(store_name, time): #return list of menu available at the time
+def get_menu(store_name, time):             #return list of menu available at the time
     if store_name in store_menu.keys():
-        if 6 <time[3] < 11:
-            x = store_menu[store_name][0]
-            print(x)
-        elif 21 >= time[3] >= 11:
+        if 6 <= time[3] <= 10:              #Morning menu
+            x = store_menu[store_name][0], store_menu[store_name][1]
+            return x
+
+        elif 11 <= time[3] <=21:            #Afternoon menu
             x = store_menu[store_name]
-            print(x)
-        else:
-            print("invalid")
+            return x
+
+        else:                               #Not within operating hours
+            print("Store is not open.")
             return 0
     else:
-        print("invalid")
+        print("Invalid")
         return 0
 
 
-#get_menu(store_name, time)
+#print(get_menu(store_name, time))          #Testing code
