0b1ef5f39e9059cc120e39236a76c7e6682c4156
tanweeli <56423902+tanweeli@users.noreply.github.com>
2019-11-10 17:22:58 +0800

    Update queuetime.py

--- queuetime.py
+++ queuetime.py
@@ -2,10 +2,14 @@ def waiting_time():
     while True:
         try:
             num_ppl = int(input("Enter no. of people in the queue: "))
-            wait_time = (int(num_ppl) + 1)*3
-            print("Estimated waiting time is ", wait_time, " minutes.")
-            break
+            if num_ppl < 0:
+                print("Please enter a non-negative integer.")
+                continue
+            else:    
+                wait_time = (int(num_ppl) + 1)*3
+                print("Estimated waiting time is ", wait_time, " minutes.")
+                break
         except:
             print("Please enter a valid no.")
             continue
-waiting_time()
+#waiting_time()
