d3a56881a782dfa46da740b1dc78d20f92ba4b7b
tanweeli <56423902+tanweeli@users.noreply.github.com>
2019-11-10 17:16:48 +0800

    Update store2.py

--- store2.py
+++ store2.py
@@ -1,31 +1,36 @@
-store_menu = {
-    "Yong Tau Foo" : ["Bee Hoon", "Kuay Teow", "Laska"],
-    "Chicken Rice" : ["Steamed Chicken Rice", "Roasted Chicken Rice"],
-    "Hand-made Noodles" : ["Sliced Fish Soup", "Ban Mian", "Tom Yam Seafood Noodles"],
-    "Cantonese Roast Duck" : ["Specialty Duck Rice", "Duck Noodles", "Roasted Pork Rice"],
-    "Western Food" : ["Chicken Chop", "Chicken Cutlet", "Fish and Chips"],
-    "Salad" : ["Chicken Salad", "Fish Salad", "Salmon Salad"],
-    "Starbuck's Coffee" : ["Americano", "Latte", "Cappucino"] }
+store_menu = { "Yong Tau Foo" : {"morning" : "Bee Hoon, Kuay Teow", "afternoon" : "Laska", "monday" : "Satay", "wednesday" : ""},
+    "Chicken Rice" : {"morning" : "Steamed Chicken Rice", "afternoon" : "Roasted Chicken Rice", "monday" : "" , "wednesday" : "Specialty Chicken Rice Set" },
+    "Hand-made Noodles" : {"morning" : "Sliced Fish Soup, Ban Mian" , "afternoon" : "Tom Yam Seafood Noodles", "monday" : "" , "wednesday" : ""},
+    "Cantonese Roast Duck" : {"morning" : "Specialty Duck Rice, Duck Noodles", "afternoon" : "Roasted Pork Rice", "monday" : "" , "wednesday" : ""},
+    "Western Food" : {"morning" : "Chicken Chop, Fish and Chips", "afternoon" : "Fish and Chips", "monday" : "" , "wednesday" : ""},
+    "Salad" : {"morning": "Chicken Salad, Garden Salad", "afternoon" : "Salmon Salad", "monday" : "" , "wednesday" : ""},
+    "Starbuck's Coffee" : {"morning" : "Americano, Latte, Cappucino" , "afternoon" :  "", "monday" : "" , "wednesday" : ""} }
 
-#store_name = "Starbuck's Coffee"           Testing code
-#time = (2019,11,7,9,29) #yyyy,mm,dd,hh,mm  Testing code
+#store_name = "Chicken Rice"                          
+#time = (2019,11,7,11,29,1)                             #yyyy,mm,dd,hh,mm,day
 
-def get_menu(store_name, time):             #return list of menu available at the time
-    if store_name in store_menu.keys():
-        if 6 <= time[3] <= 10:              #Morning menu
-            x = store_menu[store_name][0], store_menu[store_name][1]
+def get_menu(store_name, time):                         #return list of menu available at the time
+    if store_name in store_menu:
+        if 6 <= time[3] <= 10 and time[5] == 1:         #Morning menu
+            x = store_menu[store_name]["morning"], store_menu[store_name]["monday"]
+            return x
+        elif 6 <= time[3] <= 10:
+            x = store_menu[store_name]["morning"]
             return x
 
-        elif 11 <= time[3] <=21:            #Afternoon menu
-            x = store_menu[store_name]
+        elif 11 <= time[3] <=21 and time[5] == 3:       #Afternoon menu
+            x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"], store_menu[store_name]["wednesday"]
             return x
-
-        else:                               #Not within operating hours
+        elif 11 <= time[3] <=21:
+            x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"]
+            return x
+        else:                                           #Not within operating hours
             print("Store is not open.")
             return 0
+    
     else:
         print("Invalid")
         return 0
 
 
-#print(get_menu(store_name, time))          #Testing code
+#print(get_menu(store_name, time))
