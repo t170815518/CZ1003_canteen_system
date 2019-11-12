6c06d220d271373872eb30cd194b86de921c7a47
tanweeli <56423902+tanweeli@users.noreply.github.com>
2019-11-12 17:37:41 +0800

    Update store2.py

--- store2.py
+++ store2.py
@@ -1,64 +1,122 @@
-store_menu = { "Yong Tau Foo" : {"morning" : "Bee Hoon, Kuay Teow", "afternoon" : "Laska", "monday" : "Satay", "wednesday" : ""},
-    "Chicken Rice" : {"morning" : "Steamed Chicken Rice", "afternoon" : "Roasted Chicken Rice", "monday" : "" , "wednesday" : "Specialty Chicken Rice Set" },
-    "Hand-made Noodles" : {"morning" : "Sliced Fish Soup, Ban Mian" , "afternoon" : "Tom Yam Seafood Noodles", "monday" : "" , "wednesday" : ""},
-    "Cantonese Roast Duck" : {"morning" : "Specialty Duck Rice, Duck Noodles", "afternoon" : "Roasted Pork Rice", "monday" : "" , "wednesday" : ""},
-    "Western Food" : {"morning" : "Chicken Chop, Fish and Chips", "afternoon" : "Fish and Chips", "monday" : "" , "wednesday" : ""},
-    "Salad" : {"morning": "Chicken Salad, Garden Salad", "afternoon" : "Salmon Salad", "monday" : "" , "wednesday" : ""},
-    "Starbuck's Coffee" : {"morning" : "Americano, Latte, Cappucino" , "afternoon" :  "", "monday" : "" , "wednesday" : ""} }
+store_menu = { "Yong Tau Foo" : {"morning" : ["Bee Hoon", "Kuay Teow"],
+                                 "afternoon" : ["Laska"],
+                                 "monday" : ["Satay"],
+                                 "wednesday" : [],
+                                 "sep" : []},
+
+    "Chicken Rice" : {"morning" : ["Steamed Chicken Rice"],
+                      "afternoon" : ["Roasted Chicken Rice"],
+                      "monday" : [] ,
+                      "wednesday" : ["Specialty Chicken Rice Set"],
+                      "sep" : [] },
+
+    "Hand-made Noodles" : {"morning" : ["Sliced Fish Soup", "Ban Mian"] ,
+                           "afternoon" : ["Tom Yam Seafood Noodles"],
+                           "monday" : [] ,
+                           "wednesday" : [],
+                           "sep" : []},
+
+    "Cantonese Roast Duck" : {"morning" : ["Specialty Duck Rice", "Duck Noodles"],
+                              "afternoon" : ["Roasted Pork Rice"],
+                              "monday" : [] ,
+                              "wednesday" : [],
+                              "sep" : []},
+
+    "Western Food" : {"morning" : ["Chicken Chop", "Fish and Chips"],
+                      "afternoon" : ["Seafood Spaghetti"],
+                      "monday" : [] ,
+                      "wednesday" : [],
+                      "sep" : []},
+
+    "Salad" : {"morning": ["Chicken Salad", "Garden Salad"],
+               "afternoon" : ["Salmon Salad"],
+               "monday" : [] ,
+               "wednesday" : [],
+               "sep" : []},
+
+    "Starbuck's Coffee" : {"morning" : ["Americano", "Latte", "Cappucino"] ,
+                           "afternoon" :  [],
+                           "monday" : [] ,
+                           "wednesday" : [],
+                           "sep" : []},
+
+    "McDonald's" : {"morning" : ["McMuffin", "Big Breakfast", "Hotcakes"],
+                    "afternoon" : ["McChicken", "McSpicy", "Fillet O' Fish"],
+                    "monday" : [] ,
+                    "wednesday" : [],
+                    "sep" : [] }}
 
 
 
-def get_menu(store_name, time):                     #return list of menu available at the time
-    if 1 <= time[5] <=5 and store_name == "Starbuck's Coffee":
-        if 7 <= time[3] <= 10:
-                x = store_menu[store_name]["morning"]
+def get_menu(store_name, TIME, mode):
+    if mode == 1:     #When mode ==1, displays all items regardless of time. mode == 0 is default value.
+        x = store_menu[store_name]["morning"] + store_menu[store_name]["afternoon"] + store_menu[store_name]["monday"] + store_menu[store_name]["wednesday"]
+        return x
+    elif store_name == "McDonald's":
+        if 1 <= TIME[5] <=6:
+            if 7 <= TIME[3] <= 11:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["sep"]
                 return x
-        elif 11 <= time[3] <= 22:
-                x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"]
+            elif 12 <= TIME[3] <= 24:
+                x = store_menu[store_name]["afternoon"] + store_menu[store_name]["sep"]
+                return x                
+        elif TIME[5] == 0:
+            if 10 <= TIME[3] <= 11:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["sep"]
                 return x
-    elif 1 <= time[5] <= 5:                                         #Weekdays
-        if time[5] == 1:                                            #Monday Special Menu
-            if 7 <= time[3] <= 10:
-                x = store_menu[store_name]["morning"], store_menu[store_name]["monday"]
+            elif 12 <= TIME[3] <= 22:
+                x = store_menu[store_name]["afternoon"] + store_menu[store_name]["sep"]
                 return x
-            elif 11 <= time[3] <= 21:
-                x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"], store_menu[store_name]["monday"]
+    elif store_name == "Starbuck's Coffee":
+        if 1 <= TIME[5] <= 5:
+            if 7 <= TIME[3] <= 22:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["sep"]
+                return x
+            else:
+                pass
+        elif TIME[5] == 0 or TIME[5] == 6:
+            if  7 <= TIME[3] <= 17:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["sep"]
+                return x
+            else:
+                pass        
+    elif 1 <= TIME[5] <= 5:                                         #Weekdays
+        if TIME[5] == 1:                                            #Monday Special Menu
+            if 7 <= TIME[3] <= 10:
+                x = store_menu[store_name]["morning"]+ store_menu[store_name]["monday"]
+                return x
+            elif 11 <= TIME[3] <= 21:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["afternoon"] + store_menu[store_name]["monday"]
                 return x    
-        elif time[5] == 3:                                          #Wednesday Special Menu
-            if 7 <= time[3] <= 10:
-                x = store_menu[store_name]["morning"], store_menu[store_name]["wednesday"]
+        elif TIME[5] == 3:                                          #Wednesday Special Menu
+            if 7 <= TIME[3] <= 10:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["wednesday"]
                 return x
-            elif 11 <= time[3] <= 21:
-                x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"], store_menu[store_name]["wednesday"]
+            elif 11 <= TIME[3] <= 21:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["afternoon"] + store_menu[store_name]["wednesday"]
                 return x    
         else:                                                       #Other weekdays
-            if 7 <= time[3] <= 10:
-                x = store_menu[store_name]["morning"]
+            if 7 <= TIME[3] <= 10:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["sep"]
                 return x
-            elif 11 <= time[3] <= 21:
-                x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"]
+            elif 11 <= TIME[3] <= 21:
+                x = store_menu[store_name]["morning"] + store_menu[store_name]["afternoon"]
                 return x
-    elif 6 <= time[5] <= 7 and store_name == "Starbuck's Coffee":
-        if 7 <= time[3] <= 10:
-            x = store_menu[store_name]["morning"]
+    elif TIME[5] == 6:                                              #Saturday
+        if 7 <= TIME[3] <= 10:
+            x = store_menu[store_name]["morning"] + store_menu[store_name]["sep"]
             return x
-        elif 11 <= time[3] <= 17:
-            x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"]
+        elif 11 <= TIME[3] <= 15:
+            x = store_menu[store_name]["morning"] + store_menu[store_name]["afternoon"]
             return x
-    elif time[5] == 6:                                              #Saturday
-        if 7 <= time[3] <= 10:
-            x = store_menu[store_name]["morning"]
-            return x
-        elif 11 <= time[3] <= 15:
-            x = store_menu[store_name]["morning"], store_menu[store_name]["afternoon"]
-            return x
-    else:                                                          #Sunday
+    else:                                                          #Sunday, most stores closed
         pass
     
 
     
 if __name__ == '__main__':
     print("test")
-    store_name = "Chicken Rice"
-    time = (2019, 11, 7, 17, 29, 1)  # yyyy,mm,dd,hh,mm,day
-    print(get_menu(store_name, time))
+    mode = 0 # 0 for normal input, 1 for displaying all menus
+    store_name = "McDonald's"
+    TIME = (2019, 11, 7, 17, 29, 1)  # yyyy,mm,dd,hh,mm,day
+    print(get_menu(store_name, TIME,mode))
